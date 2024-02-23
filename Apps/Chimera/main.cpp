19beae57c2762bcd77bba93353547f8e09b8d1cf
gukazma <gukazma@proton.me>
2024-02-23 10:59:05 +0800

    ✨ feat: 去掉不需要的界面

--- Apps/Chimera/main.cpp
+++ Apps/Chimera/main.cpp
@@ -6,11 +6,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "MainWindow.hpp"
+#include "MainWindow.h"
 
 #include <QApplication>
 
-using namespace features_pair_demo;
 
 int main( int argc, char **argv )
 {
